05c6527867d1f3fbc5b42f040f527f8ebc7ec675
Andrej <andrej.1998@uns.ac.rs>
2021-06-02 16:41:32 +0200

    Profile service - fixed repository

--- profile/model/profile.go
+++ profile/model/profile.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type Profile struct {
 	gorm.Model
 	Username string `json:"username" gorm:"unique;not null"`
-	ProfileSettings ProfileSettings `json:"profileSettings" gorm:"foreignKey:ID"`
-	PersonalData PersonalData `json:"personalData" gorm:"foreignKey:ID"`
+	ProfileSettings ProfileSettings `json:"profileSettings"`
+	PersonalData PersonalData `json:"personalData"`
 	Biography string `json:"biography"`
 	Website string `json:"website"`
 	Type ProfileType `json:"type"`
