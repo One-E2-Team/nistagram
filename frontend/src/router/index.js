d60e52749708b3e1b5d8d81d82cb49be63062dbd
BozidarArsic <arsicbozidar98@gmail.com>
2021-06-03 12:54:00 +0200

    preapare import page frontend

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import CreatePost from '../components/CreatePost.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
@@ -16,7 +16,16 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import ( /* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/post',
+    name: 'Post',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: CreatePost
   }
 ]
 
