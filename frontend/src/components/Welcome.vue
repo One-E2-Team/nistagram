6fd5ff574fa8fe07c84307214ac406eca4b3fbff
BozidarArsic <arsicbozidar98@gmail.com>
2021-06-05 00:35:25 +0200

    created home page frontend

---
+++ frontend/src/components/Welcome.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Nistagram
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          For a lot of nudity, fake profiles and other things you can’t do without 
+          <br>please join us
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <v-row justify="center">
+         <v-btn
+                color="success"
+                class="mr-4"
+                elevation="8"
+                large
+                @click="login"
+                >
+                Log in
+                </v-btn>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <v-btn
+                color="primary"
+                class="mr-4"
+                elevation="8"
+                large
+                @click="register"
+                >
+                Register
+                </v-btn>
+         <v-img
+          :src="require('../assets/minions.jpg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    methods: {
+      login(){
+        this.$router.push({ name: 'Login' })
+      },
+      register(){
+        this.$router.push({ name: 'Register' })
+      }
+    }
+  }
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Welcome />
+    <v-img
+          :src="require('../assets/minions.jpg')"
+          class="my-3"
+          contain
+          height="200"
+        />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Welcome from '@/components/Welcome.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Welcome
   }
 }
 </script>
