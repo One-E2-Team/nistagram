3952bc1b83a7879a2a3849914eafeb589f864c62
Radoš Milićev <40705899+rammba@users.noreply.github.com>
2021-06-07 21:33:26 +0200

    send follow request from front

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -3,6 +3,13 @@
         <v-row align="left" >
             <v-col cols="12" sm="12" >
                 <personal-data style="height:200px"   />
+                <v-btn v-if="isMyProfile"
+                color="warning"
+                elevation="8"
+                @click="follow"
+                >
+                Follow
+                </v-btn>
             </v-col>
         </v-row>
     </v-container>
@@ -10,9 +17,27 @@
 
 <script>
 import PersonalData from '../components/PersonalData.vue'
+import axios from 'axios'
+import * as comm from '../configuration/communication.js'
 export default {
     components: {
         PersonalData
+    },
+    data: () => ({
+      isMyProfile: true,
+      profileId: 1,
+    }),
+    methods: {
+        follow(){
+            axios({
+                method: "post",
+                url: 'http://' + comm.server + '/api/connection/following/request/' + this.profileId,
+            }).then(response => {
+              if(response.status==200){
+                  alert('Success');
+              }
+            })
+        }
     }
 }
 </script>
