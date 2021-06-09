413231aecfe08cf06b7af425544af3a45a67270e
Andrej <andrej.1998@uns.ac.rs>
2021-06-09 17:01:35 +0200

    Profile service - registration validation

--- profile/dto/registrationDto.go
+++ profile/dto/registrationDto.go
@@ -2,15 +2,15 @@ package dto
 
 type RegistrationDto struct {
 	Username     string   `json:"username" validate:"required"`
-	Password     string   `json:"password" validate:"required"`
+	Password     string   `json:"password" validate:"required,common_pass,weak_pass"`
 	Name         string   `json:"name" validate:"required"`
 	Surname      string   `json:"surname" validate:"required"`
 	Email        string   `json:"email" validate:"required,email"`
-	Telephone    string   `json:"telephone" validate:"required"`
-	Gender       string   `json:"gender" validate:"required"`
-	BirthDate    string   `json:"birthDate" validate:"required"`
+	Telephone    string   `json:"telephone"`
+	Gender       string   `json:"gender"`
+	BirthDate    string   `json:"birthDate"`
 	IsPrivate    bool     `json:"isPrivate"`
-	Biography    string   `json:"biography" validate:"required"`
+	Biography    string   `json:"biography"`
 	WebSite      string   `json:"website"`
 	InterestedIn []string `json:"interestedIn"`
 }
