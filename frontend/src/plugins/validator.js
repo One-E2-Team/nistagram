9aecd4d86a421019de3efc176f5bef2b7ec6faef
BozidarArsic <arsicbozidar98@gmail.com>
2021-06-09 20:21:57 +0200

    register.vue validation frontend

--- frontend/src/plugins/validator.js
+++ frontend/src/plugins/validator.js
@@ -4,4 +4,5 @@ export let rules = {
   email: v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
   name: v => (v && v.length <= 10) || 'Name must be less than 10 characters',
   emailMatch: () => (`The email and password you entered don't match`),
+  password: v => new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[*.!@#$%^&(){}\\[\\]:;<>,.?~_+-=|\\/])[A-Za-z0-9*.!@#$%^&(){}\\[\\]:;<>,.?~_+-=|\\/]{8,}$').test(v) || 'Password must contain at least one lower, one capital letter, one number and one special character! Password must have at least 8 characters!'
 }
