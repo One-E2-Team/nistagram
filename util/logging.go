17f5239561506270ddb96749842adbd71015c0f5
Andrej <andrej.1998@uns.ac.rs>
2021-06-11 17:04:28 +0200

    Util - logs rotation

--- util/logging.go
+++ util/logging.go
@@ -17,6 +17,8 @@ const (
 	ERROR
 )
 
+const LogSize = 104857600
+
 func (e LogType) ToString() string {
 	switch e {
 	case INFO:
@@ -40,20 +42,27 @@ func GetLoggingStringFromID(id uint) string {
 	return "profileId: '" + Uint2String(id) + "'"
 }
 
-func Logging(logType LogType, resourceMethod string, resourceIP string, content string, service string) {
-	logFIle := "../../logs/" + service + "/"
+func getLogFileString(logType LogType) string{
 	switch logType {
 	case INFO:
-		logFIle += "infoLogs"
+		return "infoLogs"
 	case WARN:
-		logFIle += "warnLogs"
+		return "warnLogs"
 	case SUCCESS:
-		logFIle += "successLogs"
+		return "successLogs"
 	case ERROR:
-		logFIle += "errorLogs"
+		return "errorLogs"
 	}
-	logFIle += ".txt"
-	file, err := os.OpenFile(logFIle, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	return ""
+}
+
+func Logging(logType LogType, resourceMethod string, resourceIP string, content string, service string) {
+	logFileService := "../../logs/" + service + "/"
+	logFile := logFileService
+	logFile += getLogFileString(logType)
+	logFile += ".txt"
+
+	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
 	defer func(file *os.File) {
 		err := file.Close()
 		if err != nil {
@@ -67,4 +76,10 @@ func Logging(logType LogType, resourceMethod string, resourceIP string, content 
 	delimiter := "|"
 	oneLog := time.Now().UTC().String() + delimiter + logType.ToString() + delimiter + resourceMethod + delimiter + resourceIP + delimiter + content
 	log.Println(oneLog)
+
+	stat, err := file.Stat()
+
+	if(stat.Size() > 1024){ //should use LogSize here
+		os.Rename(logFile, logFileService + getLogFileString(logType) + time.Now().UTC().String() + ".txt")
+	}
 }
