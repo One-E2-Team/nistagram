1d81aacd1353251a6afd0fc9804b77a44e25a97b
Igor Šikuljak <41073725+FmasterofU@users.noreply.github.com>
2021-06-17 14:56:12 +0200

    bugfix on connection endpoint

--- connection/main.go
+++ connection/main.go
@@ -82,8 +82,8 @@ func handleFunc(handler *handler.Handler) {
 		util.RBAC(handler.GetAllFollowRequests, "READ_CONNECTION_REQUESTS", true)).Methods("GET") //frontend func
 	router.HandleFunc("/connection/following/request/{profileId}",
 		util.RBAC(handler.DeclineFollowRequest, "EDIT_CONNECTION_STATUS", false)).Methods("DELETE") //frontend func
-	router.HandleFunc("connection/block/{profileId}", handler.BlockProfile).Methods("PUT")
-	router.HandleFunc("connection/mute/{profileId}", handler.MuteProfile).Methods("PUT")
+	router.HandleFunc("/connection/block/{profileId}", handler.BlockProfile).Methods("PUT")
+	router.HandleFunc("/connection/mute/{profileId}", handler.MuteProfile).Methods("PUT")
 		fmt.Println("Starting server..")
 	host, port := util.GetConnectionHostAndPort()
 	var err error
