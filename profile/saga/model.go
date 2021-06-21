be6fb04674a96d0ab97e0f5e24775fab3d66a014
hi-imAndy <andrej.1998@uns.ac.rs>
2021-06-21 14:46:00 +0200

    orchestrator init & mb init

---
+++ profile/saga/model.go
@@ -0,0 +1,18 @@
+package saga
+
+import (
+	"encoding/json"
+	"nistagram/profile/model"
+)
+
+type Message struct {
+	NextService		string		    `json:"nextService"`
+	SenderService   string          `json:"sender_service"`
+	Action          string          `json:"action"`
+	Functionality   string		    `json:"functionality"`
+	Profile			model.Profile   `json:"profile"`
+}
+
+func (m Message) MarshalBinary() ([]byte, error) {
+	return json.Marshal(m)
+}
---
+++ profile/saga/orchestrator.go
@@ -0,0 +1,65 @@
+package saga
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"os"
+	"time"
+)
+
+const (
+	ProfileChannel string = "ProfileChannel"
+	AuthChannel string = "AuthChannel"
+	ConnectionChannel string = "ConnectionChannel"
+	PostChannel string = "PostChannel"
+	ReplyChannel    string = "ReplyChannel"
+	ActionStart     string = "Start"
+	ActionRollback  string = "RollbackMsg"
+	ActionDone      string = "DoneMsg"
+	ActionError     string = "ErrorMsg"
+)
+
+type Orchestrator struct{
+	Client *redis.Client
+	PubSub *redis.PubSub
+}
+
+func NewOrchestrator() (*Orchestrator, error){
+	var client *redis.Client
+	time.Sleep(5 * time.Second)
+	var redisHost, redisPort = "localhost", "6379"          // dev.db environment
+	_, ok := os.LookupEnv("DOCKER_ENV_SET_PROD")        // production environment
+	if ok {
+		redisHost = "message_broker"
+		redisPort = "6379"
+	} else {
+		_, ok := os.LookupEnv("DOCKER_ENV_SET_DEV") // dev front environment
+		if ok {
+			redisHost = "message_broker"
+			redisPort = "6379"
+		}
+	}
+	for {
+		client = redis.NewClient(&redis.Options{
+			Addr:     redisHost + ":" + redisPort,
+			Password: "",
+			DB:       0,
+		})
+
+		if err := client.Ping(context.TODO()).Err(); err != nil {
+			fmt.Println("Cannot connect to redis! Sleeping 10s and then retrying....")
+			time.Sleep(10 * time.Second)
+		} else {
+			fmt.Println("Connected to redis.")
+			break
+		}
+	}
+
+	orch := &Orchestrator{
+		Client: client,
+		PubSub: client.Subscribe(context.TODO(), ProfileChannel, AuthChannel, ConnectionChannel, PostChannel),
+	}
+
+	return orch, nil
+}
