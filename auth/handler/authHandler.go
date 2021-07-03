805a74f9349f6700b654233962677edc6a235549
BozidarArsic <arsicbozidar98@gmail.com>
2021-07-04 00:04:48 +0200

    auth tracing

--- auth/handler/authHandler.go
+++ auth/handler/authHandler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
@@ -17,17 +18,24 @@ type AuthHandler struct {
 }
 
 func (handler *AuthHandler) LogIn(w http.ResponseWriter, r *http.Request) {
+	span := util.Tracer.StartSpanFromRequest("LogIn-handler", r)
+	defer util.Tracer.FinishSpan(span)
+
+	util.Tracer.LogFields(span, "handler", fmt.Sprintf("handling %s\n", r.URL.Path))
 	var req dto.LogInDTO
+	ctx := util.Tracer.ContextWithSpan(context.Background(), span)
 	err := json.NewDecoder(r.Body).Decode(&req)
 	methodPath := "nistagram/auth/handler.LogIn"
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		util.Logging(util.ERROR, methodPath, "", err.Error(), "auth")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	var user *model.User
-	user, err = handler.AuthService.LogIn(req)
+	user, err = handler.AuthService.LogIn(ctx, req)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		util.Logging(util.WARN, methodPath, util.GetIPAddress(r), err.Error(), "auth")
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -35,6 +43,7 @@ func (handler *AuthHandler) LogIn(w http.ResponseWriter, r *http.Request) {
 
 	token, err := util.CreateToken(user.ProfileId, "auth_service")
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		util.Logging(util.ERROR, methodPath, "", err.Error(), "auth")
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -48,6 +57,7 @@ func (handler *AuthHandler) LogIn(w http.ResponseWriter, r *http.Request) {
 	}
 	respJson, err := json.Marshal(resp)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		util.Logging(util.ERROR, methodPath, "", err.Error(), "auth")
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -59,20 +69,29 @@ func (handler *AuthHandler) LogIn(w http.ResponseWriter, r *http.Request) {
 }
 
 
-func (handler *AuthHandler) LogInAgentAPI(writer http.ResponseWriter, request *http.Request) {
+func (handler *AuthHandler) LogInAgentAPI(writer http.ResponseWriter, r *http.Request) {
+	span := util.Tracer.StartSpanFromRequest("LogInAgentAPI-handler", r)
+	defer util.Tracer.FinishSpan(span)
+
+	util.Tracer.LogFields(span, "handler", fmt.Sprintf("handling %s\n", r.URL.Path))
+
 	var login dto.APILoginDTO
-	err := json.NewDecoder(request.Body).Decode(&login)
+	err := json.NewDecoder(r.Body).Decode(&login)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		writer.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	resp, err := handler.AuthService.AgentLoginAPIToken(login)
+	ctx := util.Tracer.ContextWithSpan(context.Background(), span)
+	resp, err := handler.AuthService.AgentLoginAPIToken(ctx, login)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		writer.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	respJson, err := json.Marshal(*resp)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		writer.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -82,16 +101,23 @@ func (handler *AuthHandler) LogInAgentAPI(writer http.ResponseWriter, request *h
 }
 
 func (handler *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
+	span := util.Tracer.StartSpanFromRequest("Register-handler", r)
+	defer util.Tracer.FinishSpan(span)
+
+	util.Tracer.LogFields(span, "handler", fmt.Sprintf("handling %s\n", r.URL.Path))
 	var registerDTO dto.RegisterDTO
 	err := json.NewDecoder(r.Body).Decode(&registerDTO)
 	registerDTO = safeRegisterDTO(registerDTO)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		fmt.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	err = handler.AuthService.Register(registerDTO)
+	ctx := util.Tracer.ContextWithSpan(context.Background(), span)
+	err = handler.AuthService.Register(ctx, registerDTO)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		fmt.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -102,16 +128,23 @@ func (handler *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 }
 
 func (handler *AuthHandler) RequestPassRecovery(w http.ResponseWriter, r *http.Request) {
+	span := util.Tracer.StartSpanFromRequest("RequestPassRecovery-handler", r)
+	defer util.Tracer.FinishSpan(span)
+
+	util.Tracer.LogFields(span, "handler", fmt.Sprintf("handling %s\n", r.URL.Path))
 	var email string
 	methodPath := "nistagram/auth/handler.RequestPassRecovery"
 	err := json.NewDecoder(r.Body).Decode(&email)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		util.Logging(util.ERROR, methodPath, "", err.Error(), "auth")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	err = handler.AuthService.RequestPassRecovery(email)
+	ctx := util.Tracer.ContextWithSpan(context.Background(), span)
+	err = handler.AuthService.RequestPassRecovery(ctx, email)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		util.Logging(util.WARN, methodPath, util.GetIPAddress(r), "'"+email+"' "+err.Error(), "auth")
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -123,16 +156,23 @@ func (handler *AuthHandler) RequestPassRecovery(w http.ResponseWriter, r *http.R
 }
 
 func (handler *AuthHandler) ChangePassword(w http.ResponseWriter, r *http.Request) {
+	span := util.Tracer.StartSpanFromRequest("ChangePassword-handler", r)
+	defer util.Tracer.FinishSpan(span)
+
+	util.Tracer.LogFields(span, "handler", fmt.Sprintf("handling %s\n", r.URL.Path))
 	var recoveryDTO dto.RecoveryDTO
 	methodPath := "nistagram/auth/handler.ChangePassword"
 	err := json.NewDecoder(r.Body).Decode(&recoveryDTO)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		util.Logging(util.ERROR, methodPath, "", err.Error(), "auth")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	err = handler.AuthService.ChangePassword(recoveryDTO)
+	ctx := util.Tracer.ContextWithSpan(context.Background(), span)
+	err = handler.AuthService.ChangePassword(ctx, recoveryDTO)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		util.Logging(util.WARN, methodPath, util.GetIPAddress(r), err.Error(), "auth")
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -144,16 +184,23 @@ func (handler *AuthHandler) ChangePassword(w http.ResponseWriter, r *http.Reques
 }
 
 func (handler *AuthHandler) UpdateUser(w http.ResponseWriter, r *http.Request) {
+	span := util.Tracer.StartSpanFromRequest("UpdateUser-handler", r)
+	defer util.Tracer.FinishSpan(span)
+
+	util.Tracer.LogFields(span, "handler", fmt.Sprintf("handling %s\n", r.URL.Path))
 	var updateUserDto dto.UpdateUserDTO
 	err := json.NewDecoder(r.Body).Decode(&updateUserDto)
 	updateUserDto = safeUpdateUserDto(updateUserDto)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		fmt.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	err = handler.AuthService.UpdateUser(updateUserDto)
+	ctx := util.Tracer.ContextWithSpan(context.Background(), span)
+	err = handler.AuthService.UpdateUser(ctx, updateUserDto)
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		fmt.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -164,9 +211,15 @@ func (handler *AuthHandler) UpdateUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (handler *AuthHandler) ValidateUser(w http.ResponseWriter, r *http.Request) {
+	span := util.Tracer.StartSpanFromRequest("ValidateUser-handler", r)
+	defer util.Tracer.FinishSpan(span)
+
+	util.Tracer.LogFields(span, "handler", fmt.Sprintf("handling %s\n", r.URL.Path))
 	vars := mux.Vars(r)
-	err := handler.AuthService.ValidateUser(vars["id"], vars["uuid"])
+	ctx := util.Tracer.ContextWithSpan(context.Background(), span)
+	err := handler.AuthService.ValidateUser(ctx, vars["id"], vars["uuid"])
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		fmt.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -178,6 +231,7 @@ func (handler *AuthHandler) ValidateUser(w http.ResponseWriter, r *http.Request)
 	_, err = fmt.Fprintf(w, "<html><head><script>window.location.href = \""+util.GetFrontProtocol()+"://"+
 		frontHost+":"+frontPort+"/web#/log-in\";</script></head><body></body></html>")
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		fmt.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -186,8 +240,14 @@ func (handler *AuthHandler) ValidateUser(w http.ResponseWriter, r *http.Request)
 }
 
 func (handler *AuthHandler) GetAgentAPIToken(w http.ResponseWriter, r *http.Request) {
-	apiToken, err := handler.AuthService.GetAgentAPIToken(util.GetLoggedUserIDFromToken(r))
+	span := util.Tracer.StartSpanFromRequest("GetAgentAPIToken-handler", r)
+	defer util.Tracer.FinishSpan(span)
+
+	util.Tracer.LogFields(span, "handler", fmt.Sprintf("handling %s\n", r.URL.Path))
+	ctx := util.Tracer.ContextWithSpan(context.Background(), span)
+	apiToken, err := handler.AuthService.GetAgentAPIToken(ctx, util.GetLoggedUserIDFromToken(r))
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		fmt.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -198,14 +258,21 @@ func (handler *AuthHandler) GetAgentAPIToken(w http.ResponseWriter, r *http.Requ
 }
 
 func (handler *AuthHandler) GetPrivileges(w http.ResponseWriter, r *http.Request) {
+	span := util.Tracer.StartSpanFromRequest("GetPrivileges-handler", r)
+	defer util.Tracer.FinishSpan(span)
+
+	util.Tracer.LogFields(span, "handler", fmt.Sprintf("handling %s\n", r.URL.Path))
 	vars := mux.Vars(r)
 	id := util.String2Uint(vars["profileId"])
-	var privileges = handler.AuthService.GetPrivileges(id)
+	ctx := util.Tracer.ContextWithSpan(context.Background(), span)
+	var privileges = handler.AuthService.GetPrivileges(ctx,id)
 	if privileges == nil || len(*privileges) == 0 {
 		var temp = make([]string, 0)
+		//TODO: nzm sta da ispisem
 		w.WriteHeader(http.StatusBadRequest)
 		err := json.NewEncoder(w).Encode(temp)
 		if err != nil {
+			util.Tracer.LogError(span, err)
 			fmt.Println(err)
 			return
 		}
@@ -213,6 +280,7 @@ func (handler *AuthHandler) GetPrivileges(w http.ResponseWriter, r *http.Request
 		w.WriteHeader(http.StatusOK)
 		err := json.NewEncoder(w).Encode(privileges)
 		if err != nil {
+			util.Tracer.LogError(span, err)
 			fmt.Println(err)
 			return
 		}
@@ -220,9 +288,15 @@ func (handler *AuthHandler) GetPrivileges(w http.ResponseWriter, r *http.Request
 }
 
 func (handler *AuthHandler) BanUser(w http.ResponseWriter, r *http.Request) {
+	span := util.Tracer.StartSpanFromRequest("BanUser-handler", r)
+	defer util.Tracer.FinishSpan(span)
+
+	util.Tracer.LogFields(span, "handler", fmt.Sprintf("handling %s\n", r.URL.Path))
 	vars := mux.Vars(r)
-	err := handler.AuthService.BanUser(util.String2Uint(vars["profileID"]))
+	ctx := util.Tracer.ContextWithSpan(context.Background(), span)
+	err := handler.AuthService.BanUser(ctx, util.String2Uint(vars["profileID"]))
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		fmt.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -233,9 +307,15 @@ func (handler *AuthHandler) BanUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (handler *AuthHandler) MakeAgent(w http.ResponseWriter, r *http.Request) {
+	span := util.Tracer.StartSpanFromRequest("MakeAgent-handler", r)
+	defer util.Tracer.FinishSpan(span)
+
+	util.Tracer.LogFields(span, "handler", fmt.Sprintf("handling %s\n", r.URL.Path))
 	vars := mux.Vars(r)
-	err := handler.AuthService.MakeAgent(util.String2Uint(vars["profileID"]))
+	ctx := util.Tracer.ContextWithSpan(context.Background(), span)
+	err := handler.AuthService.MakeAgent(ctx, util.String2Uint(vars["profileID"]))
 	if err != nil {
+		util.Tracer.LogError(span, err)
 		fmt.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
